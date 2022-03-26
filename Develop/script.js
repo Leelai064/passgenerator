3e248c74648b2e9a262e83d8deaffeec72da89b9
Leelai Hayslett <Leelai.Hayslett@gmail.com>
2022-03-26 13:03:02 -0400

    Added prompts for the user to use lowercase,uppercase, numeric, and/or special characters

--- Develop/script.js
+++ Develop/script.js
@@ -10,9 +10,9 @@ function writePassword() {
 //Generating our button
 generateBtn.addEventListener("clickbutton",writePassword);
 // Write password to the #password input
-//Add a prompt for the user after activating the button
+/*Add a prompt for the user after activating the button*/
 var characters = prompt("Please insert a password with a minimum of 8 characters and a maximum of 128");
-// Using if else statement will prompt the user to properly follow the guidelines for their password that we have stated above.
+/* Using if else statement will prompt the user to properly follow the guidelines for their password that we have stated above.*/
 
 if(characters<8){
   alert("Please enter a password with a minimum of 8 characters");
@@ -21,13 +21,47 @@ if (characters > 128){
   alert("Please enter a passoword with a maximum of 128 characters");
 }
 if(characters > 8){
-  console.log('I would like ${characters} in my final password')
+  console.log('I would like ${characters} in my final password');
 }
-// Now that our length constraints are set the user will have solidified their character
+/* Now that our length constraints are set the user will have solidified their character*/
+
+// Now to prompt the user for special characters
+var special_character =true;
+var promptTwo = prompt("If you would like to add special characters to make your password more secure please press the ok button");
+//if else statements are needed for the user to decide if they want special characters
+if(promptTwo === special_character){
+  console.log('I would like to add special characters to my password');
+} else{
+  console.log('I would not like to make my passoword more secure with special characters')
+}
+//The user may like to add variety to their password by adding upper and lower case characters into their combination
+var lowercase =true;
+var PromptThree= confirm();
+
+if(PromptThree === lowercase){
+  console.log('I want lowercase characters in my password');
+}else {
+  console.log('I DO NOT want lowercase characters in my password');
+}
+
+var uppercase= true;
+var PromptFour=confirm();
+
+if(PromptFour === uppercase){
+  console.log('I want uppercase characters in my password');
+} else{
+  console.log('I DO NOT want uppercase characters in my password');
+}
+//Might need to add a four loop here
 
 
+//The user may or may not have wanted special characters but do they want numbers?
 
-// Below is the prompt I think would allow the user to have the computer make a pass. From there how do I make it randomly generate? (for loop of course and if else statement?)
-// var userpass = prompt("Generate a password or create your own?");
-// Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+var  numeric_character = true;
+var promptFive = prompt("Would you like to include numeric characters in your password to make it more secure? If so click the ok button");
+
+if(promptFive === numeric_character){
+  console.log('I would like numeric characters')
+} else{
+  console.log('I would not like to make my password more secure with numeric characters');
+}
