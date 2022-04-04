63b80f5581414b0dafad2444d2681b5f5986efe9
Leelai Hayslett <Leelai.Hayslett@gmail.com>
2022-04-04 01:50:53 -0400

    Updated README project description. Moved variables outside of generate password function

--- README.md
+++ README.md
@@ -1,23 +1,26 @@
-# Password Generator Project
+# Password Generator
 
-## Project Information
+## Project Decription
 
 ```
-Many websites require the user to create a username and password to for their profile. As a security measure websites encourage the user to make a case sensitive password to protect their information from other parties. This project showcases my skill in creating a password generator with special characters and length criteria. Applying this skill to future projects will allow future users to feel safe knowing that their account is secure.
+Many websites require the user to create a username and password for their profile. As a security measure websites encourage the user to make a case sensitive password to protect their information from other parties. I wanted to better understand the logic behind if else statements, for loops, variables, functions. That being said, as I went through this project, I was introduced to Math.floor and Math.random for the first time. By using both functions with my logic I learned how to utilize user based decisions to create a code that responds to their needs. All in all seeing the work it takes to create user respondant events can be applied to future user-interactive projects.
 ```
 ## Project Information
 ```
-This project was mentally exhausting. My logic is on the right path but my execution could use some work. I intend to keep expanding my knowledge on java, but I feel as if my lack in background knowledge in java has let me down here. I could not get the generate button to start the prompts. Got the prompts to work but after making some edits they no longer work either. I went through a lot of bumps with this project and put a lot of time into it. My last bump in the road was not being able to even deploy the sad site. This project was frustrating, and I feel like I put my best and I will continue to until my site works properly, but until then I feel defeated.
+
 ```
-## URL of Deployed Application
+## Deployed Application
 ```
 https://leelai064.github.io/passgenerator/
 ```
-## URL of Github Repository
+## passgenerator Github Repository
 ```
 https://github.com/Leelai064/passgenerator
 ```
-## Website Visuals
+## Website Usage
 ```
-The website looks exactly like the demo...
+Prompt example:
+
+Output Example:
+
 ```
--- script.js
+++ script.js
@@ -1,86 +1,60 @@
-var generateBtn = document.querySelector = ("#generate");
+var generateBtn = document.querySelector("#generate");
 
+//variables for characters chose in arrays!
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var special_character = ["~","!", "@", "#", "$","%","^","&","*","-","+"];
+var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","v","w","x","y","z"];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G","H","I","J","K","L","M","N","O","P","Q","R","S","T","V","W","X","Y","Z"];
+
+ //  b. lowercase, uppercase, numbers, special characters
+ var confirmnum = confirm("Would you like your password to contain numbers? If yes, click ok! If no, click cancel");
+ var confirmspec= confirm("Would you like your password to contain special characters? If yes, click ok! If no, click cancel");
+ var confirmlower = confirm("Would you like your password to contain lower case characters? If yes, click ok! If no, click cancel");
+ var confirmupper = confirm("Would you like your password to contain upper case characters? If yes, click ok! If no, click cancel");
+
+  //2. Validate the input: Collects answers based on the users selections
+  var passchoice = [];
+
+//function generates password
+function generatePassword(){
+  console.log("The button has been clicked");
+  //1. Prompt the user for the password criteria
+  //  a. Password length 8< 128
+    
+  var length = prompt("Please insert a password with a minimum of 8 characters and a maximum of 128");
+  if (length > 8 || length < 128){
+    alert(`Your password contains ${length} characters`);
+  } 
+  if(length < 8 || length > 128){
+    alert('Your password must be between 8-128 characters!');
+  }
+  //3. Generate password base on criteria.
+ if (confirmnum){
+   passchoice.push(...number);
+ }
+ if(confirmspec){
+   passchoice.push(...special_character);
+ }
+ if(confirmlower){
+   passchoice.push(...lowercase);
+ }
+ if(confirmupper){
+   passchoice.push(...uppercase)
+ }
+
+
+
+//4. Display password to the page. (our return is already complete)
+  return "Generated Password Complete!";
+}
+//Write password to the #password input
 function writePassword(){
   var password = generatePassword();
-  var passwordtext = document.querySelector("#password");
+  var passwordText = document.querySelector("#password");
 
-  passwordtext.value = password;
+  passwordText.value = password;
+  
 }
+//Add event listener to generate button
 
 generateBtn.addEventListener("click",writePassword);
-
-//variable declaration 
-
-var length;
-var special_character;
-var numeric_character;
-var uppercase;
-var lowercase;
-
-//Value
-var number = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
-var special_character = ("~","!", "@", "#", "$","%","^","&","*","-","+");
-var lowercase = ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","v","w","x","y","z");
-var uppercase = ("A", "B", "C", "D", "E", "F", "G","H","I","J","K","L","M","N","O","P","Q","R","S","T","V","W","X","Y","Z");
-//confirm password length
-
-var length = prompt("Please insert a password with a minimum of 8 characters and a maximum of 128");
-console.log(length)
-if(length > 8 || length <128){
-  alert('Your password contains ${length} amount of characters');
-}
-else if(length < 8 || length > 128){
-  alert('Your passowrd must be between 8-128 chacarter length!')
-}
-// Simplified my if else statements by utilizing or
-
-//nested every other customization under this function
-function generatePassword(){
-
-  console.log("Lets make a password!");
-
-// Now to prompt the user for special characters
-var special_character =true;
-var promptTwo = prompt("If you would like to add special characters to make your password more secure please press the ok button");
-//if else statements are needed for the user to decide if they want special characters
-if(promptTwo === special_character){
-  console.log('I would like to add special characters to my password');
-} else{
-  console.log('I would not like to make my passoword more secure with special characters')
-}
-//The user may like to add variety to their password by adding upper and lower case characters into their combination
-var PromptThree= confirm();
-
-if(PromptThree === lowercase){
-  console.log('I want lowercase characters in my password');
-}else {
-  console.log('I DO NOT want lowercase characters in my password');
-}
-
-var PromptFour=confirm();
-
-if(PromptFour === uppercase){
-  console.log('I want uppercase characters in my password');
-} else{
-  console.log('I DO NOT want uppercase characters in my password');
-}
-//The user may or may not have wanted special characters but do they want numbers?
-var promptFive = prompt("Would you like to include numeric characters in your password to make it more secure? If so click the ok button");
-
-if(promptFive === numeric_character){
-  console.log('I would like numeric characters')
-} else{
-  console.log('I would not like to make my password more secure with numeric characters');
-}
-
-// My tutor suggested I use Math.floor and Math.random
-for (var i=0;i < length; i++){
-
-// state variable selecteddecision, decision, decision.length, push
-  var selecteddecision= decision[Math.floor(Math.random() * decision.length)];
-  password.push(selecteddecision);
-}
-
-  return "You have genreated a password!"
-}
-//Might need to add a four loop here
